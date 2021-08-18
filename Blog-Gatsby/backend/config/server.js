ce6b94b1b3dd12e92d77e1d37e00b484facbf0e4
Melanie Almeida <almeida.melanie@gmail.com>
2021-08-18 21:50:02 +0200

    Installing Gatsby starter

---
+++ Blog-Gatsby/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7be5f190bf5068f4292ffbfc92b935df'),
+    },
+  },
+});
---
+++ Blog-Gatsby/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '75931a57-00b6-44ef-b7d6-8fcecfddcd3f'
+};
